d638ed8ad70dd9e2fbf905a6944627b065b17a9b
yogendratamang48 <yogen48@hotmail.com>
2016-05-13 11:26:13 +0545

    Create User Added!

--- Authenticate.cs
+++ Authenticate.cs
@@ -28,3 +28,47 @@ protected void btnLogin_Click(object sender, EventArgs e)
             }
 
         }
+		
+		
+		protected void CreateUser(tblEmployee employee)
+        {
+            int result = 0;
+            string conStr = ConfigurationManager.ConnectionStrings["EmployeeConnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                con.Open();
+                string checkSQL = "sp_Check_User";
+                string SQL = "sp_insert_User";
+                SqlCommand cmdCheck = new SqlCommand(checkSQL, con);
+                SqlCommand CMD = new SqlCommand(SQL, con);
+                cmdCheck.CommandType = CommandType.StoredProcedure;
+                string password = new EmployeeMGRConfig().Encryption(employee.fldMobileNo);
+                CMD.CommandType = CommandType.StoredProcedure;
+
+                cmdCheck.Parameters.AddWithValue("@UserName", employee.fldPersonalEmail);
+
+                if (Convert.ToInt32(cmdCheck.ExecuteScalar()) == 1)
+                {
+
+
+                    CMD.Parameters.AddWithValue("@fldEmployeeId", employee.fldEmployeeID);
+                    CMD.Parameters.AddWithValue("@fldLoginName", employee.fldMobileNo);
+                    CMD.Parameters.AddWithValue("@fldUsername", employee.fldPersonalEmail);
+                    CMD.Parameters.AddWithValue("@fldPassword", password);
+                    CMD.Parameters.AddWithValue("@fldIsActive", 0);
+                    CMD.Parameters.AddWithValue("@fldCreatedBy", "Admin");
+                    CMD.Parameters.AddWithValue("fldUserType", "staff");
+                    CMD.Parameters.AddWithValue("@fldCreatedDate", DateTime.Now);
+                    CMD.Parameters.AddWithValue("@fldUpdatedBy", "Admin");
+                    CMD.Parameters.AddWithValue("@fldUpdatedDate", DateTime.Now);
+                    CMD.Parameters.AddWithValue("@fldNote", " ");
+                    result = CMD.ExecuteNonQuery();
+                }
+                else
+                {
+                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('UserName Already Taken!')", true);
+                }
+                CMD.Connection.Close();
+                CMD = null;
+            }
+        }
